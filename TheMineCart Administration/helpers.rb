0743002d367ade4ec665c748399120fd59bc9543
Jason Berry <uniqueberr@gmail.com>
2012-06-23 18:10:12 -0400

    Improving calculation for averageMinutesPlayed

--- TheMineCart Administration/helpers.rb
+++ TheMineCart Administration/helpers.rb
@@ -1,3 +1,5 @@
+require 'Date'
+
 helpers do
   SECONDS_IN_A_DAY = 86400
 
@@ -5,13 +7,16 @@ helpers do
     Time.parse(time)
   end
 
+  def parseDate(time)
+    Date.parse(time)
+  end
+
   def formatTime(time)
     parseTime(time).strftime("%m/%d/%Y - %l:%M %p %z")
   end
 
-
   def averageMinutesPlayed(joinedOn, minutesPlayed)
-    daysSinceJoinDate = (Time.now - parseTime(joinedOn))/SECONDS_IN_A_DAY
+    daysSinceJoinDate = Date.today - parseDate(joinedOn)
     (Integer(minutesPlayed)/daysSinceJoinDate).to_i
   end
 end
